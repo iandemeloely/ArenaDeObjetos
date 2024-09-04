2346a9f9972cf3cce2221297eafe1709a127b5b8
iandemeloely <iandemeloely@gmail.com>
2024-09-04 12:06:41 -0300

    Arena

--- src/Model/Ladrao.java
+++ src/Model/Ladrao.java
@@ -1,6 +1,38 @@
 package Model;
 
-public class Ladrao {
+public class Ladrao extends Pessoa {
+    private String planodefuga;
+
+    public Ladrao() {
+
+    }
+
+    public Ladrao(String nome, String cabelo, String olho, boolean sexo, String planodefuga) {
+        super(nome, cabelo, olho, sexo);
+        this.planodefuga = planodefuga;
+    }
+    public String getplanodefuga() {
+        return planodefuga;
+    }
+    public void setplanodefuga(String panodefuga){
+        this.planodefuga = panodefuga;
+    }
+
+    public String fugir() {
+        return "da no pé";
+    }
+
+    public String roubar(){
+        return "Arrastão";
+    }
+
+    @Override
+    public String toString() {
+        return "Ladrao [getNome()=" + getNome() + ", getplanodefuga()=" + getplanodefuga() + ", getCabelo()="
+                + getCabelo() + ", getOlho()=" + getOlho() + ", getPontosDeVida()=" + getPontosDeVida()
+                + ", getClass()=" + getClass() + "]";
+    }
+    
 
     
 
